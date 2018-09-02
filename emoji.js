52dd62c64227c95f43bf41f5990058e6303fcfec
pax <pax@mioritics.ro>
2018-09-02 21:37:46 +0300

    record history to localStorage

--- emoji.js
+++ emoji.js
@@ -4,6 +4,34 @@
 
 */
 
+if (localStorage.getItem("activity")) {
+  // console.log('has storage');
+  zz=JSON.parse(localStorage.getItem("activity"));
+  var sortable = [];
+  for (var element in zz) {
+      sortable.push([element, zz[element]]);
+  }
+  sortable.sort(function(a, b) {
+    return b[1] - a[1];
+  });
+  var display=''
+  // console.log(sortable)
+  for (var element in sortable) {
+    // zx+='x' + element;
+    console.log(sortable[element][0])
+    display += '<span>' + sortable[element][0] + '<sub> '  + sortable[element][1] +  '</sub></span>';
+  }
+
+
+// console.log(zx)
+  document.getElementById("activity").innerHTML=display;
+}
+else {
+  console.log('localStorage.getItem("activity") = null' );
+}
+
+
+
 
 // https://ourcodeworld.com/articles/read/188/encode-and-decode-html-entities-using-pure-javascript
 function encode (str) {
@@ -45,7 +73,7 @@ function showinfo(){
 }
 
 function copyToClipboard(targetID) {
-// console.log(this)
+
 var target= document.getElementById(targetID);
   /* Select the text field */
   target.select();
@@ -56,6 +84,16 @@ document.getElementById("msg").innerHTML='copied';
 document.getElementById("msg").classList.toggle('show');
 document.getElementById("msg").classList.toggle('hide');
 
+
+// var storedActivity = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+var storedActivity = localStorage.getItem("activity") ? JSON.parse(localStorage.getItem("activity")) : {};
+storedActivity[target.value] = storedActivity[target.value] ? storedActivity[target.value] + 1 : 1;
+// storedActivity['this']=storedActivity[target.value];
+console.log(storedActivity);
+localStorage.setItem("activity", JSON.stringify(storedActivity));
+// localStorage.clear();
+// console.log(storedActivity);
+// console.log(target.value + ' → localStorage');
 }
 
 
